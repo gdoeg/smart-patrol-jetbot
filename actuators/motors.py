120a99e1adfb903d9672c0a11e0671c55659011f
gcasta11 <gcasta@uw.edu>
2026-03-16 02:50:31 -0700

    Rewire forward and backward motors

--- actuators/motors.py
+++ actuators/motors.py
@@ -10,7 +10,9 @@ else:
 
 
 class MotorController:
+
     def __init__(self):
+
         print("Initializing Adafruit MotorHAT motor driver...", flush=True)
 
         if Adafruit_MotorHAT is None:
@@ -19,8 +21,15 @@ class MotorController:
             )
 
         self.mh = Adafruit_MotorHAT(addr=0x60, i2c_bus=1)
+
         self.left_motor = self.mh.getMotor(1)
         self.right_motor = self.mh.getMotor(2)
+
+        # MOTOR ORIENTATION FIX
+        # If robot moves backwards when told forward,
+        # invert both motors here
+        self.left_dir = -1
+        self.right_dir = -1
 
         print("Adafruit MotorHAT motor driver initialized.", flush=True)
 
@@ -29,78 +38,64 @@ class MotorController:
         speed = max(0.0, min(1.0, float(speed)))
         return int(speed * 255)
 
-    # --------------------------------
-    # Direct wheel speed control
-    # --------------------------------
+    def _run_motor(self, motor, direction):
+
+        if direction > 0:
+            motor.run(Adafruit_MotorHAT.FORWARD)
+        elif direction < 0:
+            motor.run(Adafruit_MotorHAT.BACKWARD)
+        else:
+            motor.run(Adafruit_MotorHAT.RELEASE)
+
     def set_speeds(self, left_speed, right_speed):
 
-        left_power = self._speed_to_power(left_speed)
-        right_power = self._speed_to_power(right_speed)
+        left_power = self._speed_to_power(abs(left_speed))
+        right_power = self._speed_to_power(abs(right_speed))
 
         self.left_motor.setSpeed(left_power)
         self.right_motor.setSpeed(right_power)
 
-        self.left_motor.run(Adafruit_MotorHAT.FORWARD)
-        self.right_motor.run(Adafruit_MotorHAT.FORWARD)
+        left_dir = 1 if left_speed >= 0 else -1
+        right_dir = 1 if right_speed >= 0 else -1
 
-    # --------------------------------
-    # Basic movements
-    # --------------------------------
+        left_dir *= self.left_dir
+        right_dir *= self.right_dir
+
+        self._run_motor(self.left_motor, left_dir)
+        self._run_motor(self.right_motor, right_dir)
+
     def forward(self, speed=0.2, duration=None):
-        power = self._speed_to_power(speed)
 
-        self.left_motor.setSpeed(power)
-        self.right_motor.setSpeed(power)
-
-        self.left_motor.run(Adafruit_MotorHAT.FORWARD)
-        self.right_motor.run(Adafruit_MotorHAT.FORWARD)
+        self.set_speeds(speed, speed)
 
         if duration:
             time.sleep(duration)
             self.stop()
 
     def backward(self, speed=0.2, duration=None):
-        power = self._speed_to_power(speed)
 
-        self.left_motor.setSpeed(power)
-        self.right_motor.setSpeed(power)
-
-        self.left_motor.run(Adafruit_MotorHAT.BACKWARD)
-        self.right_motor.run(Adafruit_MotorHAT.BACKWARD)
+        self.set_speeds(-speed, -speed)
 
         if duration:
             time.sleep(duration)
             self.stop()
 
     def left(self, speed=0.2, duration=None):
-        power = self._speed_to_power(speed)
 
-        self.left_motor.setSpeed(power)
-        self.right_motor.setSpeed(power)
-
-        self.left_motor.run(Adafruit_MotorHAT.BACKWARD)
-        self.right_motor.run(Adafruit_MotorHAT.FORWARD)
+        self.set_speeds(-speed, speed)
 
         if duration:
             time.sleep(duration)
             self.stop()
 
     def right(self, speed=0.2, duration=None):
-        power = self._speed_to_power(speed)
 
-        self.left_motor.setSpeed(power)
-        self.right_motor.setSpeed(power)
-
-        self.left_motor.run(Adafruit_MotorHAT.FORWARD)
-        self.right_motor.run(Adafruit_MotorHAT.BACKWARD)
+        self.set_speeds(speed, -speed)
 
         if duration:
             time.sleep(duration)
             self.stop()
 
-    # --------------------------------
-    # Emergency stop
-    # --------------------------------
     def stop(self):
 
         self.left_motor.setSpeed(0)
